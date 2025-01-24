ec6536924ecb6eac7d8a44298516c2be2f501dd1
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-24 23:10:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Generate all binary strings/generate-all-binary-strings.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    void generateStrings(int n, string current, vector<string>& result) {
+        // Base case: if the string has reached length N, add it to the result
+        if (current.size() == n) {
+            result.push_back(current);
+            return;
+        }
+
+        // Add '0' and recurse
+        generateStrings(n, current + "0", result);
+
+        // Add '1' only if the last character is not '1'
+        if (current.empty() || current.back() != '1') {
+            generateStrings(n, current + "1", result);
+        }
+    }
+
+    vector<string> generateBinaryStrings(int N) {
+        vector<string> result;
+        generateStrings(N, "", result);
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+
+    while(t--){
+        //Input
+        int n; cin >> n;
+
+        Solution obj;
+        
+        vector<string> v = obj.generateBinaryStrings(n);
+        for(auto it:v) cout<<it<<" ";
+        cout << endl;
+    
+cout << "~" << "\n";
+}
+
+    return 0;
+}
+
+// } Driver Code Ends
