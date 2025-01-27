b537d57e3d1ebb9d5647020bf6ef03f02241372f
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-01-27 23:58:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count total set bits/count-total-set-bits.cpp
+++ Difficulty: Medium/Count total set bits/count-total-set-bits.cpp
@@ -8,25 +8,23 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
-public:
-    // Function to return the sum of count of set bits in the integers from 1 to n.
-    int countSetBits(int n) {
-        if (n == 0) return 0;
-        
-        // Find the highest power of 2 less than or equal to n
-        int x = log2(n);
-        
-        // Number of set bits in numbers from 1 to (2^x - 1)
-        int bitsUpto2PowerX = x * (1 << (x - 1));
-        
-        // Set bits in the MSB of numbers from (2^x) to n
-        int msbBitsFrom2PowerXToN = n - (1 << x) + 1;
-        
-        // Recursively calculate set bits for the remainder
-        int remainder = n - (1 << x);
-        
-        return bitsUpto2PowerX + msbBitsFrom2PowerXToN + countSetBits(remainder);
+class Solution{
+    public:
+    
+    // n: input to count the number of set bits
+    //Function to return sum of count of set bits in the integers from 1 to n.
+    int countSetBits(int N)
+    {
+        // Your logic here
+        int two = 2,ans = 0;
+        int n = N;
+        while(n){
+            ans += (N/two)*(two>>1);
+            if((N&(two-1)) > (two>>1)-1) ans += (N&(two-1)) - (two>>1)+1;
+            two <<= 1;
+            n >>= 1;
+        }
+    return ans;
     }
 };
 
