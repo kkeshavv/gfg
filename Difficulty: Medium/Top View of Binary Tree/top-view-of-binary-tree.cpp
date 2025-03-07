e61addd308eb9f4152c3c1bb33633535fe4b016f
keshav <112716732+kkeshavv@users.noreply.github.com>
2025-03-08 00:19:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -95,44 +95,44 @@ struct Node
     Node* right;
 };
 */
+
+
 class Solution {
   public:
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
     vector<int> topView(Node *root) {
         // code here
+        
         vector<int>ans;
-        if(!root) return ans;
-        
-        
-        map<int,int>mpp;
         queue<pair<Node*,int>>q;
-        
+        map<int,int>mpp;
+        if(!root) return ans;
         q.push({root,0});
         while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            Node*node=it.first;
-            int line=it.second;
-            
-            
-            if(mpp.find(line)==mpp.end()){
-                mpp[line]=node->data;
-            }
-            
-            if(node->left){
-                q.push({node->left,line-1});
-            }
-            if(node->right){
-                q.push({node->right,line+1});
-            }
+           auto it=q.front();
+           q.pop();
+           
+           Node*node=it.first;
+           int line=it.second;
+           
+           
+           if(mpp.find(line)==mpp.end()){
+               mpp[line]=node->data;
+           }
+           if(node->left){
+               q.push({node->left,line-1});
+           }
+           if(node->right){
+               q.push({node->right,line+1});
+           }
         }
         
-        for(auto it: mpp){
+        for(auto it:mpp){
             ans.push_back(it.second);
         }
-        return ans;
         
+        return ans;
     }
 };
 
